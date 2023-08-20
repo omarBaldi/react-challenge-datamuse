df72acce2404f6ad3aee9ec64af09d871d0d4885
Omar Baldi <omar.baldi@maersk.com>
2023-08-20 20:23:38 +0200

    Created custom hook to handle promise state

---
+++ src/hooks/usePromise.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const usePromise = <T>({ promiseFn }: { promiseFn: () => Promise<T> }) => {
+  const promiseFnToExecute = useRef<typeof promiseFn>(promiseFn);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasErrorOccurred, setHasErrorOccurred] = useState<boolean>(false);
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    promiseFnToExecute.current = promiseFn;
+  }, [promiseFn]);
+
+  const executePromise = (): void => {
+    setIsLoading(true);
+
+    promiseFnToExecute
+      .current()
+      .then(setData)
+      .catch(() => setHasErrorOccurred(true))
+      .finally(() => setIsLoading(false));
+  };
+
+  return {
+    isLoading,
+    hasErrorOccurred,
+    data,
+    executePromise,
+  };
+};
