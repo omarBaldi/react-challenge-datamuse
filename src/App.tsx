04f7f456a1a0067c26b8bb4146698f07eda5c1ee
Omar Baldi <omar.baldi@maersk.com>
2023-08-20 19:51:06 +0200

    Added input text element to DOM and ensure 2-way data binding with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
+/**
+ * @description
+ * TODO: add input box element to the DOM
+ * TODO: implement debounce logic that will be used to call API endpoint
+ * TODO: create axios instance + create API service to get synonyms based on key searched
+ * TODO: render results in DOM
+ * TODO: create custom hook to handle API state (loading, error, data)
+ */
 function App() {
-  return <></>;
+  const [inputBoxValue, setInputBoxValue] = useState<string>('');
+
+  const handleInputBoxChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const updatedInputBoxValue = e.target.value;
+    setInputBoxValue(updatedInputBoxValue);
+  };
+
+  return (
+    <>
+      <input
+        type='text'
+        value={inputBoxValue}
+        placeholder='Get synonyms...'
+        onChange={handleInputBoxChange}
+      />
+    </>
+  );
 }
 
 export default App;
