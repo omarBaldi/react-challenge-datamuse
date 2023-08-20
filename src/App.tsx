44d3e6f97042739f4174275fef5fa35fb9dd9458
Omar Baldi <omar.baldi@maersk.com>
2023-08-20 20:10:40 +0200

    Integrated logic into app to call API endpoint and render list of synonyms into DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { useDebouncedValue } from './hooks/useDebouncedValue';
+import { getSynonyms } from './services/get-synonyms';
 
 /**
  * @description
- * TODO: add input box element to the DOM
- * TODO: implement debounce logic that will be used to call API endpoint
- * TODO: create axios instance + create API service to get synonyms based on key searched
  * TODO: render results in DOM
  * TODO: create custom hook to handle API state (loading, error, data)
  */
 function App() {
   const [inputBoxValue, setInputBoxValue] = useState<string>('');
+  const debouncedInputBoxValue = useDebouncedValue(inputBoxValue);
+  const [synonyms, setSynonyms] = useState<string[]>([]);
 
   const handleInputBoxChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const updatedInputBoxValue = e.target.value;
     setInputBoxValue(updatedInputBoxValue);
   };
+
+  useEffect(() => {
+    if (!debouncedInputBoxValue) return;
+
+    getSynonyms({ keyToSearch: debouncedInputBoxValue })
+      .then(setSynonyms)
+      .catch(console.error);
+  }, [debouncedInputBoxValue]);
 
   return (
     <>
@@ -25,6 +34,11 @@ function App() {
         placeholder='Get synonyms...'
         onChange={handleInputBoxChange}
       />
+
+      {synonyms.length > 0 &&
+        synonyms.map((s) => {
+          return <p key={s}>{s}</p>;
+        })}
     </>
   );
 }
