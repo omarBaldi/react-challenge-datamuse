4293cc0b39d6c81fe754cfe3ecaad589ba871c79
Omar Baldi <omar.baldi@maersk.com>
2023-08-21 09:21:11 +0200

    Refactoring of custom promise hook by replacing multiple useState hooks with useReducer for centralized state data

--- src/hooks/usePromise.ts
+++ src/hooks/usePromise.ts
@@ -1,24 +1,42 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useReducer, useRef } from 'react';
+
+type ApiState<T> = {
+  isLoading: boolean;
+  hasErrorOccurred: boolean;
+  data: T | undefined;
+};
 
 export const usePromise = <T>({ promiseFn }: { promiseFn: () => Promise<T> }) => {
   const promiseFnToExecute = useRef<typeof promiseFn>(promiseFn);
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [hasErrorOccurred, setHasErrorOccurred] = useState<boolean>(false);
-  const [data, setData] = useState<T>();
+  const initialState: ApiState<T> = {
+    isLoading: false,
+    hasErrorOccurred: false,
+    data: undefined,
+  };
+
+  const [{ isLoading, hasErrorOccurred, data }, setApiState] = useReducer(
+    (state: typeof initialState, newState: Partial<typeof initialState>) => {
+      return {
+        ...state,
+        ...newState,
+      };
+    },
+    initialState
+  );
 
   useEffect(() => {
     promiseFnToExecute.current = promiseFn;
   }, [promiseFn]);
 
   const executePromise = useCallback(() => {
-    setIsLoading(true);
+    setApiState({ isLoading: true });
 
     promiseFnToExecute
       .current()
-      .then(setData)
-      .catch(() => setHasErrorOccurred(true))
-      .finally(() => setIsLoading(false));
+      .then((data) => setApiState({ data }))
+      .catch(() => setApiState({ hasErrorOccurred: true }))
+      .finally(() => setApiState({ isLoading: false }));
   }, []);
 
   return {
